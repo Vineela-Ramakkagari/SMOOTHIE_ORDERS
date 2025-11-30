9a371ecd529908c266b3dd9fba28a5b3355e5d5c
Vineela-Ramakkagari <122731858+Vineela-Ramakkagari@users.noreply.github.com>
2025-11-30 16:50:41 +0530

    Update streamlit_app.py
    Converted snowpark dataframe to pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 
@@ -19,8 +20,14 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Getting only Fruit Name colum into DF
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data = my_dataframe, use_container_width = True)
+# st.stop()
+
+# Convert the snowpark df to pandas df to use loc function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 # Dropdown menu to choose upto 5 fruit names
 ingredients_list = st.multiselect(
